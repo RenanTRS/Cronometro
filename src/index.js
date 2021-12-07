497c045fae5d2ccab46944694c4b6f968c6e4e5c
Renan Souza <renantrsouza@gmail.com>
2021-12-07 14:23:43 -0400

    Add reset

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import {Reset} from './styles/reset';
+
 ReactDOM.render(
   <React.StrictMode>
+    <Reset />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
