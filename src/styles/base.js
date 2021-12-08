993ca8fbe881f098cb3e0ea3288dbbec1589dc2d
Renan Souza <renantrsouza@gmail.com>
2021-12-08 11:19:54 -0400

    change font-family

--- src/styles/base.js
+++ src/styles/base.js
@@ -5,11 +5,10 @@ export const Base = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-
-    font-family: 'Inter', sans-serif;
 }
 
 :root{
     font-size: 62.5%;
+    font-family: 'Inter', sans-serif;
 }
 `;
