04586953dc7e0246d962d637f73ce40a86afab37
Renan Souza <renantrsouza@gmail.com>
2021-12-23 16:18:35 -0400

    add media querie mobile landscape

--- src/styles/media.js
+++ src/styles/media.js
@@ -1,5 +1,6 @@
 export const media = {
     mobile: '@media (max-width: 767.98px)',
+    mobileLandscape: '@media (max-width: 767.98px) and (orientation: landscape)',
     tablet: '@media (min-width: 768px) and (max-width: 1199.98px)',
     desktop: '@media (min-width: 1200px)'
 }
