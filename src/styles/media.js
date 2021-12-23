c5c90e36608d50d2efc6fa5aae501521da139bb0
Renan Souza <renantrsouza@gmail.com>
2021-12-23 15:35:37 -0400

    create media queries

---
+++ src/styles/media.js
@@ -0,0 +1,5 @@
+export const media = {
+    mobile: '@media (max-width: 767.98px)',
+    tablet: '@media (min-width: 768px) and (max-width: 1199.98px)',
+    desktop: '@media (min-width: 1200px)'
+}
