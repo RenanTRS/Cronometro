6e19f86a58d208fd8e8ac83cb6977d4002f3a874
Renan Souza <renantrsouza@gmail.com>
2021-12-24 04:20:44 -0400

    add media landscape for tablet

--- src/styles/media.js
+++ src/styles/media.js
@@ -2,5 +2,6 @@ export const media = {
     mobile: '@media (max-width: 767.98px)',
     mobileLandscape: '@media (max-width: 767.98px) and (orientation: landscape)',
     tablet: '@media (min-width: 768px) and (max-width: 1199.98px)',
+    tabletLandscape: '@media (min-width: 768px) and (max-width: 1199.98px) and (orientation: landscape)',
     desktop: '@media (min-width: 1200px)'
 }
